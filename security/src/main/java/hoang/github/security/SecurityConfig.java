4cb432cca14e7228c6c0f17c59c5e73036c4c68e
Hoang Nguyen <hoangng1005@gmail.com>
2021-03-07 23:49:43 +0700

    security

--- security/src/main/java/hoang/github/security/SecurityConfig.java
+++ security/src/main/java/hoang/github/security/SecurityConfig.java
@@ -1,27 +1,55 @@
 package hoang.github.security;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.tomcat.util.security.MD5Encoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class SecurityConfig {
-    @Bean
-    public UserDetailsService userDetailsService(){
-        var userDetailsService = new InMemoryUserDetailsManager();
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-        var user = User.withUsername("hoang@gmail.com").password("hoang123");
-        authorities("read").build();
+    @Autowired
+    private PasswordEncoder encoder;
 
-        userDetailsService.createUser(user);
-        return userDetailsService;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic(); // Có thể thay bằng   http.formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
     }
 
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+        //return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        Collection<UserDetails> users = new ArrayList<>();
+        UserBuilder userBuilder = User.builder().passwordEncoder(encoder::encode);
+        var tom = userBuilder.username("tom@gmail.com").password("123").roles("USER").build();
+        var bob = userBuilder.username("bob@gmail.com").password("123").roles("USER").build();
+        var alice = userBuilder.username("alice@gmail.com").password("123").roles("USER").build();
+
+        System.out.println(alice.getPassword());
+
+        users.add(tom);
+        users.add(bob);
+        users.add(alice);
+        return new InMemoryUserDetailsManager(users);
     }
 }
---
+++ security/src/main/java/hoang/github/security/SecurityConfig0.java
@@ -0,0 +1,26 @@
+package hoang.github.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig0 {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("hoang@gmail.com").password("hoang123").authorities("read").build();
+
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
+
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
